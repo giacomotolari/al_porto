a0b6f284636374423d48c76bff03ed398af1aee4
giacomotolari <tolarigiacomo@gmail.com>
2024-03-09 12:47:54 +0100

    feat: paths

---
+++ src/features/navigation/layout/paths.ts
@@ -0,0 +1,17 @@
+interface Paths {
+  home: string;
+  book: string;
+  favorites: string;
+  bookmark: string;
+  about: string;
+  notFound: string;
+}
+
+const paths: Paths = {
+  home: "/",
+  book: "/book",
+  favorites: "/favorites",
+  bookmark: "/bookmark",
+  about: "/about",
+  notFound: "/404",
+};
