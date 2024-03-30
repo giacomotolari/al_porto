236aa650e8bccb5fb3590e3f9950eb489e1702f3
giacomotolari <tolarigiacomo@gmail.com>
2024-03-30 11:19:53 +0100

    style: menu width with laptop vire

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import {
   IonApp,
-  IonRouterOutlet,
-  IonSplitPane,
-  setupIonicReact,
+  setupIonicReact
 } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import { Redirect, Route } from "react-router-dom";
-import Menu from "./features/navigation/layout/menu/Menu";
-import Home from "./pages/Home";
-import Book from "./pages/Book";
-import Favorites from "./pages/Favorites";
-import About from "./pages/About";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -21,16 +12,16 @@ import "@ionic/react/css/structure.css";
 import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
+import "@ionic/react/css/display.css";
+import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/padding.css";
 import "@ionic/react/css/text-alignment.css";
 import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import "./theme/variables.css";
 import Layout from "./features/navigation/layout/Layout";
+import "./theme/variables.css";
 
 setupIonicReact();
 
